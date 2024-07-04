15c9a0d226e0f979f1c40bc6aba20ec81110ea49
jamy11 <jamyarrahman82@gmail.com>
2024-07-04 12:29:56 -0230

    reducer created

--- frontend/src/redux/features/segmentation-slice.js
+++ frontend/src/redux/features/segmentation-slice.js
@@ -1,4 +1,49 @@
 import axiosInstance from "@/axios/axiosInstance";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+  segmentedImage: null,
+  status: "idle",
+  error: null,
+};
+
+export const segmentImage = createAsyncThunk(
+  "segmentation/segmentImage",
+  async (file, thunkAPI) => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await axiosInstance.post("/segment/", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      return response.data.segmented_image;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const segmentationSlice = createSlice({
+  name: "segmentation",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(segmentImage.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(segmentImage.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.segmentedImage = action.payload;
+      })
+      .addCase(segmentImage.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      });
+  },
+});
+
+export default segmentationSlice.reducer;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import predictionSlice from "./features/prediction-slice";
+import segmentationSlice from "./features/segmentation-slice";
 
 export const store = configureStore({
   reducer: {
     prediction: predictionSlice,
+    segmentation: segmentationSlice,
   },
 });
