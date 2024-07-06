0d8dcadda2a3963d5f81ffd61f91caf8c0ed030a
jamy11 <jamyarrahman82@gmail.com>
2024-07-06 13:15:23 -0230

    Image Segmentation design done

--- frontend/src/components/ImageSegmentation.js
+++ frontend/src/components/ImageSegmentation.js
@@ -46,20 +46,27 @@ const ImageSegmentation = () => {
         </>
       )} */}
 
-      <Center height="100vh">
-        <Box
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          height="100%"
-        >
-          <Image
+      <div className="p-8 max-w-screen-md mx-auto">
+        {/* Title */}
+        <h1 className="text-center text-3xl font-bold mb-4">
+          Image Segmentation
+        </h1>
+
+        {/* Image with Effects */}
+        <div className="relative rounded-lg overflow-hidden mb-8">
+          <img
             src={"https://bit.ly/dan-abramov"}
-            alt="Image Segmentation"
-            boxSize="sm"
+            alt="Segmented Image"
+            className="w-full h-auto transition-transform duration-500 transform hover:scale-110"
           />
-        </Box>
-      </Center>
+          <div className="absolute inset-0 bg-black opacity-0 hover:opacity-25 transition-opacity duration-500"></div>
+        </div>
+
+        {/* Description or Additional Information */}
+        <div className="text-center text-gray-600">
+          <p>This is the segmented image.</p>
+        </div>
+      </div>
     </div>
   );
 };
